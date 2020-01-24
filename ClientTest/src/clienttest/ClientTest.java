a6c628f162e0d7d90f01d66d7d54021bbda4f7f4
awaddell77 <waddell.andrew@gmail.com>
2020-01-23 16:12:14 -0800

    Updates

--- ClientTest/src/clienttest/ClientTest.java
+++ ClientTest/src/clienttest/ClientTest.java
@@ -6,6 +6,7 @@
 package clienttest;
 
 import java.io.IOException;
+import java.io.*;
 import java.net.*;
 
 /**
@@ -17,17 +18,24 @@ public class ClientTest {
     /**
      * @param args the command line arguments
      */
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        for (int i = 0; i < 1; i++){
+            connect();
+            System.out.println("Attempt #"+i);
+        }
+    }
+    public static void connect() throws IOException {
        // String hostname = "192.168.99.226";
        String hostname = "192.168.99.249";
        boolean h = true;
        int attempts = 0;
         Socket socket = null;
-        while(h){
+        
         try {
-            socket = new Socket(hostname, 125);
+            socket = new Socket(hostname, 3210);
             socket.setSoTimeout(30000);
+            attempts++;
+            
             
             //INPUT STUFF GOES HERE
             //InputStream in = socket.getInputStream();
@@ -36,6 +44,27 @@ public class ClientTest {
             System.err.println(ex);
         }finally {
             if (socket != null){
+                
+                System.out.println("Connection made!");
+                InputStream in = socket.getInputStream();
+                Writer out = new OutputStreamWriter(socket.getOutputStream(), "ascii");
+                //OutputStreamWriter writer = new OutputStreamWriter(out);
+                out.write("report");
+                out.flush();
+                InputStreamReader reader = new InputStreamReader(in);
+                StringBuilder test = new StringBuilder();
+                for (int c = reader.read(); c != -1; c= reader.read()){
+                    //System.out.println(Character.toString((char) c));
+                    System.out.println((char)c);
+                    test.append((char)c);
+                    test.append('-');
+                    
+                    
+                }
+                System.out.print("MESSAGE: " + test);
+                out.write("EXIT");
+                out.flush();
+                h = false;
                 try {
                     socket.close();
                     
@@ -43,12 +72,11 @@ public class ClientTest {
                     //ignore
                 }
             }
-            if (attempts > 5){
-                h = false;
+        
                 
-            }
+            
         }
     }
     
-    }
+    
 }
